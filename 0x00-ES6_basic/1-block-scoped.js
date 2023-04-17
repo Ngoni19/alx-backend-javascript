28d1d7617cb529d8a0d03f8672f852d8f445d24c
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-04-18 01:41:16 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-/* use const for hoisting */
+
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // no-unused-vars
-    const task2 = false; // no-unused-vars
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
