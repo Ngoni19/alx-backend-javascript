36939f6f9d0afc2c5f97d046f1e663a17422dbeb
Ngoni19 <ngoni19@live.com>
2023-04-17 23:01:03 +0000

    block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* use const for hoisting */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // no-unused-vars
+    const task2 = false; // no-unused-vars
+  }
+
+  return [task, task2];
+}
