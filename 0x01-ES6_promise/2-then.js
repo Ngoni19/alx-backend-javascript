0da06a7ba2964ea15485c421b9b86f4d62dcf99c
Ngoni19 <ngoni19@live.com>
2023-04-23 12:14:51 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,10 @@
-
 function handleResponseFromAPI(promise) {
   const body = { status: 200, body: 'success' };
 
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return body;
-    })
-    .catch((error) => {
-      console.log('Got an error from the API');
-      return error;
-    });
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
-
 export default handleResponseFromAPI;
 
