51bed132094515b7771e7afb288ad87deb3df880
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-02 00:37:25 +0200

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,12 +2,20 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
-get displayFullPrice() {
-    return ${ this.amount } ${ this.currency.displayFullCurrency() };
+  get amount() { return this._amount; }
+
+  get currency() { return this._currency; }
+
+  set amount(amount) { this._amount = amount; }
+
+  set currency(currency) { this._currency = currency; }
+
+  displayFullPrice() {
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
