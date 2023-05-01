c0728c94f9c6089cbe1c78ff7d73a3fc6720b4c0
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-02 00:14:22 +0200

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,4 +4,5 @@ export default class Pricing { constructor(amount, currency) { this.amount = amo
 
 get displayFullPrice() { return ${this.amount} ${this.currency.displayFullCurrency()}; }
 
-static convertPrice(amount, conversionRate) { return amount * conversionRate; } }
+static convertPrice(amount, conversionRate) { return amount * conversionRate; } 
+}
