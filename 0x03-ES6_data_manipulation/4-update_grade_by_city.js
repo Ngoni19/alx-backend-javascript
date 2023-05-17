b9a6ac4cc9110cb8c39bbe591a0a2fb2f96cc892
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-17 17:05:18 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,8 @@
-export default function updateStudentGradesByCity(arr = [], city, newGrades = []) {
+export default function getStudentGradesByCity(arr = [], city, newGrades = []) {
   return arr
     .filter(student => student.location === city)
     .map(student => {
       const grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+      return { ...student, grade: grade?.grade || 'N/A' };
     });
 }
-
-
