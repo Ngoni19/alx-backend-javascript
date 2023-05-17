7628e6424af45d031071ba0c3beb34e1f57907cf
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-17 16:35:37 +0200

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,11 +1,3 @@
 export default function getStudentsByLocation(arr, city) {
-  const studentsInCity = [];
-
-  for (const student of arr) {
-    if (student.location === city) {
-      studentsInCity.push(student);
-    }
-  }
-
-  return studentsInCity;
+  return arr.filter((student) => student.location === city);
 }
