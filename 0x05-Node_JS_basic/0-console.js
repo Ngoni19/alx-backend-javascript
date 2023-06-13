a300c676097b6b1333b1a8ff8cf7406115ba5aa7
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-06-13 22:32:41 +0200

    Create 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ngoni19 <https://github.com/Ngoni19>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
