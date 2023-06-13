508ff3c35b5d3aa586beccc5630fcb9670265d82
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-06-13 22:50:38 +0200

    Create 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Ngoni19 <https://github.com/Ngoni19>
+ */
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileL = fs
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGrps = {};
+  const dbFieldNames = fileL[0].split(',');
+  const stdPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileL.slice(1)) {
+    const studentR = line.split(',');
+    const studentPV = studentR.slice(0, studentR.length - 1);
+    const field = studentR[studentR.length - 1];
+    if (!Object.keys(studentGrps).includes(field)) {
+      studentGrps[field] = [];
+    }
+    const studentEntries = stdPropNames
+      .map((propName, idx) => [propName, studentPV[idx]]);
+    studentGrps[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGrps)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGrps)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
+};
+
+module.exports = countStudents;
